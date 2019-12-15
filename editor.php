6562e0688242575a09c52e85f8a345f9c75a701c
Bryce <boylebryce@gmail.com>
2019-12-15 12:51:21 -0800

    add basic image deletion

--- editor.php
+++ editor.php
@@ -6,14 +6,30 @@
 
     $current_tags = '';
 
-    // add add new submitted tags to tags table and apply to image by updating imagetags table
-    if (isset($_POST['img_id']) && isset($_POST['tags'])) {
+    /* To do:
+        High priority:
+        * Handle img_id doesn't exist - consider restructuring control flow to check for this first
+        * Add error handling, especially to deletions
+
+        Low priority:
+        * Consider changing all POST[img_id] to GET[img_id] by altering form action url?
+    */
+
+    /* Script Control flow:
+        if POST[add_tags] and POST[img_id]          -> add tags to image
+        if POST[delete_tags] and POST[img_id]       -> delete tags from image
+        if POST[delete_img] and POST[img_id]        -> delete image
+        if POST[img_id] or GET[img_id]              -> display image and tags
+    */
+
+    // add tags to image
+    if (isset($_POST['add_tags']) && isset($_POST['img_id'])) {
         $img_id = $_POST['img_id'];
         try {
             $db = new PDO($dsn, $db_user, $db_pw);
 
             // check if any tags are new to tag system and add to tags table
-            $tags = explode(' ', $_POST['tags']);
+            $tags = explode(' ', $_POST['add_tags']);
             foreach ($tags as $tag) {
                 $query = 'SELECT COUNT(1) AS total FROM `tags` WHERE `tag_label` = :tag';
                 $statement = $db->prepare($query);
@@ -64,9 +80,9 @@
         }
     }
 
-    // remove tags from image by removing img_id, tag_id pair from imagetags table
-    if (isset($_POST['selected_tags'])) {
-        foreach ($_POST['selected_tags'] as $tag) {
+    // delete tags from image
+    if (isset($_POST['delete_tags']) && isset($_POST['img_id'])) {
+        foreach ($_POST['delete_tags'] as $tag) {
             try {
                 $db = new PDO($dsn, $db_user, $db_pw);
                 $query = 'DELETE FROM `imagetags` WHERE `imgID` = :img_id AND `tagID` = :tag_id';
@@ -81,7 +97,49 @@
         }
     }
 
-    // get img_path and existing tags to display on page
+    // delete image
+    /*
+        To do:
+            * Remove all entries in imagetags table
+            * Delete image file on server
+            * Remove entry from images table
+    */
+    if (isset($_POST['delete_image']) && isset($_POST['img_id'])) {
+        $img_id = $_POST['img_id'];
+        $deletion_result = '';
+
+        try {
+            $db = new PDO($dsn, $db_user, $db_pw);
+
+            // Remove all entries in imagetags table
+            $query = 'DELETE FROM `imagetags` WHERE `imgID` = :img_id';
+            $statement = $db->prepare($query);
+            $statement->bindValue(':img_id', $img_id);
+            $statement->execute();
+
+            // Delete image file on server
+            $query = 'SELECT `img_path` FROM `images` WHERE `img_id` = :img_id';
+            $statement = $db->prepare($query);
+            $statement->bindValue(':img_id', $img_id);
+            $statement->execute();
+            $img_path = $statement->fetch()['img_path'];
+            unlink('img/' . $img_path);
+
+            // Remove entry from images table
+            $query = 'DELETE FROM `images` WHERE `img_id` = :img_id';
+            $statement = $db->prepare($query);
+            $statement->bindValue(':img_id', $img_id);
+            $statement->execute();
+            
+            $statement->closeCursor();
+            $deletion_result = 'Image successfully deleted.';
+        }
+        catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    // display image and tags
     if (isset($_POST['img_id']) || isset($_GET['img_id'])) {
         $img_id = isset($_POST['img_id']) ? $_POST['img_id'] : $_GET['img_id'];
         $img_path = isset($_POST['img_path']) ? $_POST['img_path'] : '';
@@ -110,7 +168,7 @@
                 $statement->bindValue(':tag_id', $tag['tagID']);
                 $statement->execute();
                 $label = $statement->fetch();
-                $current_tags .= '<a href="images.php?search=' . $label['tag_label'] . '"><li><input type="checkbox" name="selected_tags[]" value="' . $label['tag_id'] . '">' . $label['tag_label'] . '</li></a>';
+                $current_tags .= '<a href="images.php?search=' . $label['tag_label'] . '"><li><input type="checkbox" name="delete_tags[]" value="' . $label['tag_id'] . '">' . $label['tag_label'] . '</li></a>';
             }
             $statement->closeCursor();
         }
@@ -133,21 +191,23 @@
     </head>
     <body>
     <?php if (isset($_SESSION['user'])) { ?>
+        <!-- Show image, tags, and editor -->
         <?php if (isset($img_id)) { ?>
+        <?php if (isset($deletion_result)) {echo '<p>' . $deletion_result . '</p>';} ?>
         <img src="<?= 'img/' . $img_path ?>">
-        <form method="POST" action="editor.php">
+        <form id="add-tags-form" method="POST" action="editor.php">
             <label>Add space-separated tags here</label>
-            <input type="text" name="tags" required>
-            <input type="text" name="img_id" value="<?= $img_id ?>" style="display:none">
-            <input type="text" name="img_path" value="<?= $img_path ?>" style="display:none">
-            <input type="submit" value="Submit">
+            <input type="text" name="add_tags" required>
+            <input type="hidden" name="img_id" value="<?= $img_id ?>">
+            <input type="hidden" name="img_path" value="<?= $img_path ?>">
+            <input type="submit" value="Submit" name="add_tags_form">
         </form>
         <div>
             <a href="index.php">Back to main</a>
             <a href="images.php">View uploaded images</a>
         </div>
         <h2>Current tags:</h2>
-        <form method="POST" action="editor.php">
+        <form id="tag-deletion-form" method="POST" action="editor.php">
             <ul>
                 <?= $current_tags ?>
             </ul>
@@ -155,6 +215,13 @@
             <input type="text" name="img_path" value="<?= $img_path ?>" style="display:none">
             <input type="submit" value="Delete selected tags">
         </form>
+        <form id="image-deletion-form" method="POST" action="editor.php">
+            <input type="submit" value="Delete image" name="delete_image">
+            <input type="hidden" name="img_id" value="<?= $img_id ?>">
+        </form>
+        <!-- No image argument, show navigation -->
+        <?php } else { ?>
+            
         <?php } ?>
     <?php } else { echo $login_form; } ?>
     </body>
