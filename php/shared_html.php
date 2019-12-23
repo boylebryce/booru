731c34044aa740ab535057f2a874c98b7e5c7c36
Bryce <boylebryce@gmail.com>
2019-12-22 22:49:46 -0800

    add head html and fix tag search name

--- php/shared_html.php
+++ php/shared_html.php
@@ -1,5 +1,18 @@
 <?php
 
+    $head_html =
+    '
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Booru</title>
+        <meta name="description" content="A booru (tag-based image board) made from scratch by someone who doesn\'t know what they\'re doing. Expect things to either be only partially implemented or outright broken.">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/index.css">
+        <script src="js/scripts.js"></script>
+    </head>
+    ';
+
     $nav_html = 
     '
     <nav>
@@ -8,7 +21,7 @@
             <a id="images-link" class="nav-button" href="images.php">Images</a>
         </div>
         <form id="search-form" method="GET" action="images.php">
-            <input id="search-form-input" type="text" name="tags" placeholder="Search for tags here">
+            <input id="search-form-input" type="text" name="search" placeholder="Search for tags here">
         </form>
         <form id="logout-form" method="POST" action="login.php">
             <input id="logout-form-submit" class="nav-button" type="submit" name="logout" type="submit" value="Logout">
