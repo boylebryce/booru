d6f46422d6acd4a4707b2469d4b71e8bd5154fac
Bryce <boylebryce@gmail.com>
2019-12-23 17:37:02 -0800

    validate img_id before performing work

--- editor.php
+++ editor.php
@@ -6,14 +6,13 @@
     require_once('php/shared_html.php');
 
     $current_tags = '';
+    $db_error = '';
 
     /* To do:
         High priority:
-        * Handle img_id doesn't exist - consider restructuring control flow to check for this first
         * Add error handling, especially to deletions
 
         Low priority:
-        * Consider changing all POST[img_id] to GET[img_id] by altering form action url?
         * How to handle no image argument - redirect to index or images?
     */
 
@@ -24,158 +23,168 @@
         if POST[img_id] or GET[img_id]              -> display image and tags
     */
 
-    // add tags to image
-    if (isset($_POST['add_tags']) && isset($_POST['img_id'])) {
-        $img_id = $_POST['img_id'];
+    // img_id must be set to do anything in the editor
+    if(isset($_POST['img_id']) || isset($_GET['img_id'])) {
+        $img_id = isset($_POST['img_id']) ? $_POST['img_id'] : $_GET['img_id'];
+        $db = new PDO($dsn, $db_user, $db_pw);
+
+        // validate img_id - must exist in images table
         try {
-            $db = new PDO($dsn, $db_user, $db_pw);
+            $query = 'SELECT COUNT(*) FROM `images` WHERE `img_id` = :img_id';
+            $statement = $db->prepare($query);
+            $statement->bindValue(':img_id', $img_id);
+            $statement->execute();
 
-            // check if any tags are new to tag system and add to tags table
-            $tags = explode(' ', $_POST['add_tags']);
-            foreach ($tags as $tag) {
-                $query = 'SELECT COUNT(1) AS total FROM `tags` WHERE `tag_label` = :tag';
-                $statement = $db->prepare($query);
-                $statement->bindValue(':tag', $tag);
-                $statement->execute();
-                $tag_count = $statement->fetch();
-
-                // tag doesn't exist in tags table, add it
-                if ($tag_count['total'] == 0) {
-                    $query = 'INSERT INTO `tags` (`tag_id`, `tag_label`, `tag_imgcount`) VALUES (NULL, :tag, 0);';
-                    $statement = $db->prepare($query);
-                    $statement->bindValue(':tag', $tag);
-                    $result = $statement->execute();
-                }
+            // img_id doesn't exist
+            if ($statement->fetchColumn() == 0) {
+                $db_error .= 'Error: img_id ' . $img_id . ' doesn\'t exist.';
             }
-
-            // add img_id, tag_id pairs to imagetags table
-            foreach ($tags as $tag) {
-                // get tag_id
-                $query = 'SELECT `tag_id` FROM `tags` WHERE `tag_label` = :tag';
-                $statement = $db->prepare($query);
-                $statement->bindValue(':tag', $tag);
-                $statement->execute();
-                $tag_id = $statement->fetch()['tag_id'];
-
-                // check that image doesn't already have tag before adding
-                $query = 'SELECT COUNT(1) AS total FROM `imagetags` WHERE `img_id` = :img_id AND `tag_id` = :tag_id';
-                $statement = $db->prepare($query);
-                $statement->bindValue(':img_id', $_POST['img_id']);
-                $statement->bindValue('tag_id', $tag_id);
-                $statement->execute();
-                $result = $statement->fetch();
-
-                // img, tag pair doesn't already exists, so add it
-                if ($result['total'] == 0) {
-                    $query = 'INSERT INTO `imagetags` (`imagetags_id`, `img_id`, `tag_id`) VALUES (NULL, :img_id, :tag_id);';
-                    $statement = $db->prepare($query);
-                    $statement->bindValue(':img_id', $_POST['img_id']);
-                    $statement->bindValue(':tag_id', $tag_id);
-                    $statement->execute();
-
-                }
-            }
-            $statement->closeCursor();
         }
         catch (PDOException $e) {
-            echo $e->getMessage();
+            $db_error .= $e->getMessage();
         }
-    }
 
-    // delete tags from image
-    if (isset($_POST['delete_tags']) && isset($_POST['img_id'])) {
-        foreach ($_POST['delete_tags'] as $tag) {
+        // add tags to image
+        if (isset($_POST['add_tags']) && $db_error === '') {
             try {
                 $db = new PDO($dsn, $db_user, $db_pw);
-                $query = 'DELETE FROM `imagetags` WHERE `img_id` = :img_id AND `tag_id` = :tag_id';
-                $statement = $db->prepare($query);
-                $statement->bindValue(':img_id', $_POST['img_id']);
-                $statement->bindValue(':tag_id', $tag);
-                $statement->execute();
+
+                // tags are passed as a space-separated string of individual tags
+                $tags = explode(' ', $_POST['add_tags']);
+
+                // check if any tags are new to tag system and add to tags table
+                foreach ($tags as $tag_label) {
+                    $query = 'SELECT COUNT(*) FROM `tags` WHERE `tag_label` = :tag';
+                    $statement = $db->prepare($query);
+                    $statement->bindValue(':tag', $tag);
+                    $statement->execute();
+
+                    // tag doesn't exist in tags table, add it
+                    if ($statement->fetchColumn() == 0) {
+                        $query = 'INSERT INTO `tags` (`tag_id`, `tag_label`, `tag_imgcount`) VALUES (NULL, :tag_label, 0);';
+                        $statement = $db->prepare($query);
+                        $statement->bindValue(':tag_label', $tag_label);
+                        $statement->execute();
+                    }
+                }
+
+                // add img_id, tag_id pairs to imagetags table
+                foreach ($tags as $tag_label) {
+                    // get tag_id
+                    $query = 'SELECT `tag_id` FROM `tags` WHERE `tag_label` = :tag_label';
+                    $statement = $db->prepare($query);
+                    $statement->bindValue(':tag_label', $tag_label);
+                    $statement->execute();
+                    $tag_id = $statement->fetch()['tag_id'];
+
+                    // check that image doesn't already have tag before adding
+                    $query = 'SELECT COUNT(1) AS total FROM `imagetags` WHERE `img_id` = :img_id AND `tag_id` = :tag_id';
+                    $statement = $db->prepare($query);
+                    $statement->bindValue(':img_id', $img_id);
+                    $statement->bindValue('tag_id', $tag_id);
+                    $statement->execute();
+                    $result = $statement->fetch();
+
+                    // img, tag pair doesn't already exists, so add it
+                    if ($result['total'] == 0) {
+                        $query = 'INSERT INTO `imagetags` (`imagetags_id`, `img_id`, `tag_id`) VALUES (NULL, :img_id, :tag_id);';
+                        $statement = $db->prepare($query);
+                        $statement->bindValue(':img_id', $img_id);
+                        $statement->bindValue(':tag_id', $tag_id);
+                        $statement->execute();
+                    }
+                }
+                $statement->closeCursor();
             }
             catch (PDOException $e) {
-                echo $e->getMessage();
+                $db_error .= $e->getMessage();
             }
         }
-    }
 
-    // delete image
-    /*
-        To do:
-            * Remove all entries in imagetags table
-            * Delete image file on server
-            * Remove entry from images table
-    */
-    if (isset($_POST['delete_image']) && isset($_POST['img_id'])) {
-        $img_id = $_POST['img_id'];
-        $deletion_result = '';
-
-        try {
-            $db = new PDO($dsn, $db_user, $db_pw);
-
-            // Remove all entries in imagetags table
-            $query = 'DELETE FROM `imagetags` WHERE `img_id` = :img_id';
-            $statement = $db->prepare($query);
-            $statement->bindValue(':img_id', $img_id);
-            $statement->execute();
-
-            // Delete image file on server
-            $query = 'SELECT `img_path` FROM `images` WHERE `img_id` = :img_id';
-            $statement = $db->prepare($query);
-            $statement->bindValue(':img_id', $img_id);
-            $statement->execute();
-            $img_path = $statement->fetch()['img_path'];
-            unlink('img/' . $img_path);
-
-            // Remove entry from images table
-            $query = 'DELETE FROM `images` WHERE `img_id` = :img_id';
-            $statement = $db->prepare($query);
-            $statement->bindValue(':img_id', $img_id);
-            $statement->execute();
-            
-            $statement->closeCursor();
-            $deletion_result = 'Image successfully deleted.';
+        // delete tags from image
+        if (isset($_POST['delete_tags']) && $db_error === '') {
+            foreach ($_POST['delete_tags'] as $tag_id) {
+                try {
+                    $db = new PDO($dsn, $db_user, $db_pw);
+                    $query = 'DELETE FROM `imagetags` WHERE `img_id` = :img_id AND `tag_id` = :tag_id';
+                    $statement = $db->prepare($query);
+                    $statement->bindValue(':img_id', $img_id);
+                    $statement->bindValue(':tag_id', $tag_id);
+                    $statement->execute();
+                }
+                catch (PDOException $e) {
+                    $db_error .= $e->getMessage();
+                }
+            }
         }
-        catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-    }
 
-    // display image and tags
-    if (isset($_POST['img_id']) || isset($_GET['img_id'])) {
-        $img_id = isset($_POST['img_id']) ? $_POST['img_id'] : $_GET['img_id'];
-        $img_path = isset($_POST['img_path']) ? $_POST['img_path'] : '';
-        try {
-            $db = new PDO($dsn, $db_user, $db_pw);
+        // delete image
+        if (isset($_POST['delete_image']) && $db_error === '') {
+            try {
+                $db = new PDO($dsn, $db_user, $db_pw);
 
-            // get img_path if not already set
-            if ($img_path === '') {
-                $query = 'SELECT * FROM `images` WHERE `img_id` = :img_id';
+                // Remove all entries in imagetags table
+                $query = 'DELETE FROM `imagetags` WHERE `img_id` = :img_id';
+                $statement = $db->prepare($query);
+                $statement->bindValue(':img_id', $img_id);
+                $statement->execute();
+
+                // Delete image file on server
+                $query = 'SELECT `img_path` FROM `images` WHERE `img_id` = :img_id';
                 $statement = $db->prepare($query);
                 $statement->bindValue(':img_id', $img_id);
                 $statement->execute();
                 $img_path = $statement->fetch()['img_path'];
-            }
+                unlink('img/' . $img_path);
 
-            // get existing tags
-            $query = 'SELECT * FROM `imagetags` WHERE `img_id` = :img_id';
-            $statement = $db->prepare($query);
-            $statement->bindValue(':img_id', $img_id);
-            $statement->execute();
-            $result = $statement->fetchAll();
-
-            foreach ($result as $tag) {
-                $query = 'SELECT * FROM `tags` WHERE `tag_id` = :tag_id';
+                // Remove entry from images table
+                $query = 'DELETE FROM `images` WHERE `img_id` = :img_id';
                 $statement = $db->prepare($query);
-                $statement->bindValue(':tag_id', $tag['tag_id']);
+                $statement->bindValue(':img_id', $img_id);
                 $statement->execute();
-                $label = $statement->fetch();
-                $current_tags .= '<a href="images.php?search=' . $label['tag_label'] . '"><li><input type="checkbox" name="delete_tags[]" value="' . $label['tag_id'] . '">' . $label['tag_label'] . '</li></a>';
+                $statement->closeCursor();
             }
-            $statement->closeCursor();
+            catch (PDOException $e) {
+                $db_error .= $e->getMessage();
+            }
         }
-        catch (PDOException $e) {
-            echo $e->getMessage();
+
+        // display image and tags
+        if ((isset($_POST['img_id']) || isset($_GET['img_id'])) && $db_error === '') {
+            $img_id = isset($_POST['img_id']) ? $_POST['img_id'] : $_GET['img_id'];
+            $img_path = isset($_POST['img_path']) ? $_POST['img_path'] : '';
+            try {
+                $db = new PDO($dsn, $db_user, $db_pw);
+
+                // get img_path if not already set
+                if ($img_path === '') {
+                    $query = 'SELECT * FROM `images` WHERE `img_id` = :img_id';
+                    $statement = $db->prepare($query);
+                    $statement->bindValue(':img_id', $img_id);
+                    $statement->execute();
+                    $img_path = $statement->fetch()['img_path'];
+                }
+
+                // get existing tags
+                $query = 'SELECT * FROM `imagetags` WHERE `img_id` = :img_id';
+                $statement = $db->prepare($query);
+                $statement->bindValue(':img_id', $img_id);
+                $statement->execute();
+                $result = $statement->fetchAll();
+
+                foreach ($result as $tag) {
+                    $query = 'SELECT * FROM `tags` WHERE `tag_id` = :tag_id';
+                    $statement = $db->prepare($query);
+                    $statement->bindValue(':tag_id', $tag['tag_id']);
+                    $statement->execute();
+                    $label = $statement->fetch();
+                    $current_tags .= '<a href="images.php?search=' . $label['tag_label'] . '"><li><input type="checkbox" name="delete_tags[]" value="' . $label['tag_id'] . '">' . $label['tag_label'] . '</li></a>';
+                }
+                $statement->closeCursor();
+            }
+            catch (PDOException $e) {
+                $db_error .= $e->getMessage();
+            }
         }
     }
 
@@ -189,37 +198,39 @@
         <?php if (isset($nav_html)) echo $nav_html; ?>
 
         <main>
-            <!-- Show image, tags, and editor -->
-            <?php if (isset($img_id)) { ?>
-                <?php if (isset($deletion_result)) {echo '<p>' . $deletion_result . '</p>';} ?>
-                <img src="<?= 'img/' . $img_path ?>">
-                <form id="add-tags-form" method="POST" action="editor.php">
-                    <label>Add space-separated tags here</label>
-                    <input type="text" name="add_tags" required>
-                    <input type="hidden" name="img_id" value="<?= $img_id ?>">
-                    <input type="hidden" name="img_path" value="<?= $img_path ?>">
-                    <input type="submit" value="Submit" name="add_tags_form">
-                </form>
-                <div>
-                    <a href="index.php">Back to main</a>
-                    <a href="images.php">View uploaded images</a>
-                </div>
-                <h2>Current tags:</h2>
-                <form id="tag-deletion-form" method="POST" action="editor.php">
-                    <ul>
-                        <?= $current_tags ?>
-                    </ul>
-                    <input type="hidden" name="img_id" value="<?= $img_id ?>">
-                    <input type="hidden" name="img_path" value="<?= $img_path ?>">
-                    <input type="submit" value="Delete selected tags">
-                </form>
-                <form id="image-deletion-form" method="POST" action="editor.php">
-                    <input type="submit" value="Delete image" name="delete_image">
-                    <input type="hidden" name="img_id" value="<?= $img_id ?>">
-                </form>
-            <!-- No image argument -->
-            <?php } else { ?>
-
+            <!-- Show database error -->
+            <?php if ($db_error !== '') { echo $db_error; } else { ?>
+                <!-- Show image, tags, and editor -->
+                <?php if (isset($img_id)) { ?>
+                    <img src="<?= 'img/' . $img_path ?>">
+                    <form id="add-tags-form" method="POST" action="editor.php">
+                        <label>Add space-separated tags here</label>
+                        <input type="text" name="add_tags" required>
+                        <input type="hidden" name="img_id" value="<?= $img_id ?>">
+                        <input type="hidden" name="img_path" value="<?= $img_path ?>">
+                        <input type="submit" value="Submit" name="add_tags_form">
+                    </form>
+                    <div>
+                        <a href="index.php">Back to main</a>
+                        <a href="images.php">View uploaded images</a>
+                    </div>
+                    <h2>Current tags:</h2>
+                    <form id="tag-deletion-form" method="POST" action="editor.php">
+                        <ul>
+                            <?= $current_tags ?>
+                        </ul>
+                        <input type="hidden" name="img_id" value="<?= $img_id ?>">
+                        <input type="hidden" name="img_path" value="<?= $img_path ?>">
+                        <input type="submit" value="Delete selected tags">
+                    </form>
+                    <form id="image-deletion-form" method="POST" action="editor.php">
+                        <input type="submit" value="Delete image" name="delete_image">
+                        <input type="hidden" name="img_id" value="<?= $img_id ?>">
+                    </form>
+                <!-- No image argument -->
+                <?php } else { ?>
+                    <p>Something went wrong: img_id is not set.</p>
+                <?php } ?>
             <?php } ?>
         </main>
     </body>
