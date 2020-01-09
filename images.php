594321624de13f52c194553d08d55f0c16f04006
Bryce <boylebryce@gmail.com>
2020-01-09 02:04:16 -0800

    make images page use JS frontend/API backend

--- images.php
+++ images.php
@@ -1,147 +1,23 @@
 <?php
 
-    require_once 'php/paths.php';
-    require_once CONFIG_FILE;
-    require_once('php/session.php');
-    require_once('php/shared_html.php');
-
-    // display images with tags in search
-    if (isset($_GET['search'])) {
-        try {
-            $db = new PDO(DSN, DB_USER, DB_PW);
-
-            // separate tags by space-delimiter
-            $raw_tags = explode(' ', $_GET['search']);
-            $search_tags = array();
-            $quote_tag = '';
-
-            foreach ($raw_tags as $raw_tag) {
-                if ($raw_tag !== '') {
-                    if ($quote_tag === '') {
-                        // raw_tag is the start of a quote-enclosed tag
-                        if ($raw_tag[0] === '"') {
-                            $quote_tag .= $raw_tag;
-                        }
-                        // raw_tag is a standard tag
-                        else {
-                            $search_tags[] = $raw_tag;
-                        }
-                    }
-                    // raw_tag is part of a quote-enclosed tag
-                    else {
-                        $quote_tag .= ' ' . $raw_tag;
-
-                        // raw_tag is closing the quote-enclosed tag
-                        if (substr($raw_tag, -1) === '"') {
-                            $quote_tag = trim($quote_tag, '"');
-                            $search_tags[] = $quote_tag;
-                            $quote_tag = '';
-                        }
-                    }
-                }
-            }
-
-            $tags = array(); // tag_id => tag_label
-            $images = array(); // img_id => img_path
-            $image_ids = array();
-            
-            // 
-
-            // get tag_id for each tag
-            foreach ($search_tags as $tag_label) {
-                $query = 'SELECT `tag_id` FROM `tags` WHERE `tag_label` = :tag_label';
-                $statement = $db->prepare($query);
-                $statement->bindValue(':tag_label', $tag_label);
-                $statement->execute();
-                $tags[$statement->fetch()['tag_id']] = $tag_label;
-            }
-
-            // get img_id for every image that match first tag_id
-            $first_tag_id = array_key_first($tags);
-            $query = 'SELECT `img_id` FROM `imagetags` WHERE `tag_id` = :tag_id';
-            $statement = $db->prepare($query);
-            $statement->bindValue(':tag_id', $first_tag_id);
-            $statement->execute();
-            $result = $statement->fetchAll();
-
-            foreach ($result as $img) {
-                $image_ids[] = $img['img_id'];
-            }
-
-            // get img_id for every image that matches tag_id
-            foreach ($tags as $tag_id => $tag_label) {
-                $eligible = array();
-                $query = 'SELECT `img_id` FROM `imagetags` WHERE `tag_id` = :tag_id';
-                $statement = $db->prepare($query);
-                $statement->bindValue(':tag_id', $tag_id);
-                $statement->execute();
-                $result = $statement->fetchAll();
-                foreach ($result as $img) {
-                    if (array_search($img['img_id'], $image_ids) !== false) {
-                        $eligible[] = $img['img_id'];
-                    }
-                }
-                $image_ids = $eligible;
-            }
-
-            // populate images array with img_path
-            foreach ($image_ids as $img_id) {
-                $query = 'SELECT `img_path` FROM `images` WHERE `img_id` = :img_id';
-                $statement = $db->prepare($query);
-                $statement->bindValue(':img_id', $img_id);
-                $statement->execute();
-                $images[$img_id] = $statement->fetch()['img_path'];
-            }
-            $statement->closeCursor();
-
-            $image_display = '<h2>Showing images with tag(s): ';
-            foreach ($tags as $tag_id => $tag_label) {
-                $image_display .= $tag_label . ' ';
-            }
-            $image_display .= '<div>';
-            foreach ($images as $img_id => $img_path) {
-                $image_display .= '<a href="editor.php?img_id=' . $img_id . '"><img src="img/' . $img_path . '"></a>';
-            }
-            $image_display .= '</div>';
-        }
-        catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    // display all images
-    else {
-        try {
-            $db = new PDO(DSN, DB_USER, DB_PW);
-            $query = 'SELECT * FROM `images`';
-            $statement = $db->prepare($query);
-            $statement->execute();
-            $result = $statement->fetchAll();
-            $statement->closeCursor();
-        }
-        catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-        
-        $image_display = '';
-        foreach ($result as $img) {
-            $image_display .= '<a href="editor.php?img_id=' . $img['img_id'] . '"><img src="img/' . $img['img_path'] . '"></a>';
-        }
-    }
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/booru/php/paths.php';
+    require_once WEB_ROOT . '/booru/php/session.php';
+    require_once WEB_ROOT . '/booru/php/shared_html.php';
 
 ?>
 
-<!doctype html>
-<html lang="en">
-    <?php if(isset($head_html)) echo $head_html; ?>
+<!DOCTYPE html>
+<html>
+    <?php if (isset($head_html)) echo $head_html; ?>
+        <script async src='/booru/js/images.js'></script>
+        <script>
+            const GET = <?php echo json_encode($_GET); ?>;
+        </script>
     </head>
     <body>
-        <?php if (isset($nav_html)) echo $nav_html; ?>
+        <?php if(isset($nav_html)) echo $nav_html; ?>
         <main>
-            <h2>Click an image to tag it</h2>
-            <div>
-                <?= $image_display ?>
-            </div>
+            <div id="images-container"></div>
         </main>
     </body>
 </html>
---
+++ js/images.js
@@ -0,0 +1,53 @@
+window.onload = init;
+
+const PROTOCOL = document.location.protocol;
+const HOSTNAME = document.location.hostname;
+const WEBROOT = PROTOCOL + '//' + HOSTNAME;
+
+function init() {
+    const imagesContainer = document.querySelector('#images-container');
+    const searchForm = document.querySelector('#search-form');
+    const searchFormInput = document.querySelector('#search-form-input');
+
+    const images = GET['search'] ? search(GET['search']) : getAllImages();
+    images.then(result => {
+        addImages(result, imagesContainer);
+    });
+
+    searchForm.addEventListener('submit', event => {
+        event.preventDefault();
+        window.location.assign(WEBROOT + '/booru/test/images.php?search=' + searchFormInput.value)
+    })
+}
+
+async function search(searchString) {
+    const response = await fetch(WEBROOT + '/booru-api/search.php?search=' + searchString);
+    return response.json();
+}
+
+async function getAllImages() {
+    const response = await fetch(WEBROOT + '/booru-api/search.php?all=true');
+    return response.json();
+}
+
+function addImage(imagePath, imageID, container) {
+    let node = document.createElement('img');
+    let link = document.createElement('a');
+    link.href = WEBROOT + '/booru/editor.php?img_id=' + imageID;
+    node.src = WEBROOT + '/booru/img/' + imagePath;
+    link.appendChild(node);
+    container.appendChild(link);
+}
+
+function addImages(images, container) {
+    if (images.length) {
+        images.forEach(image => {
+            addImage(image['img_path'], image['img_id'], container);
+        });
+    }
+    else {
+        let node = document.createElement('p');
+        node.textContent = 'No images found';
+        container.appendChild(node);
+    }
+}
