56a4d1ff3f06ce8537a6a3a7a98f31e062053bfb
Bryce <boylebryce@gmail.com>
2020-01-09 02:03:32 -0800

    make stylesheet path absolute for portability

--- php/shared_html.php
+++ php/shared_html.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Booru</title>
         <meta name="description" content="A booru (tag-based image board) made from scratch by someone who doesn\'t know what they\'re doing. Expect things to either be only partially implemented or outright broken.">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="/booru/css/style.css">
     ';
 
     $nav_html = 
